ea5ba03770f0b4cc7f900a56e96d3d9ff78f6853
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-17 11:32:10 +0200

    :lipstick: Better footer style

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -9,9 +9,27 @@ interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => (
   <div className={classes.footer}>
-    <MailLogo width={28} height={28} className={[classes.logo, classes.logo__mail].join(' ')} />
-    <GithubLogo width={28} height={28} className={classes.logo} />
-    <LinkedinLogo width={28} height={28} className={classes.logo} />
+    <a title="Mail" href="mailto:d.benfouzari@gmail.com" className={classes.logo_wrapper}>
+      <MailLogo width={28} height={28} className={[classes.logo, classes.logo__mail].join(' ')} />
+    </a>
+    <a
+      title="Github"
+      target="_blank"
+      href="https://github.com/dbenfouzari"
+      rel="noreferrer"
+      className={classes.logo_wrapper}
+    >
+      <GithubLogo width={28} height={28} className={classes.logo} />
+    </a>
+    <a
+      title="LinkedIn"
+      target="_blank"
+      href="https://linkedin.com/in/dbenfouzari"
+      rel="noreferrer"
+      className={classes.logo_wrapper}
+    >
+      <LinkedinLogo width={28} height={28} className={classes.logo} />
+    </a>
   </div>
 );
 
